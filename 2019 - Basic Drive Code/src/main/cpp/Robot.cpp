b619211bf446b3b82bff1d3eb3170406ed4ea17a
Maximo Cazares <Maximo43873@gmail.com>
2019-02-03 14:24:53 -0800

    Update Robot.cpp

--- 2019 - Basic Drive Code/src/main/cpp/Robot.cpp
+++ 2019 - Basic Drive Code/src/main/cpp/Robot.cpp
@@ -1,199 +1,96 @@
-#include "frc/WPILib.h"
-#include "frc/RobotDrive.h"
-#include "frc/Joystick.h"
-#include <Commands/Command.h>
-#include <Commands/Scheduler.h>
-#include <LiveWindow/LiveWindow.h>
-#include <SmartDashboard/SendableChooser.h>
-#include <SmartDashboard/SmartDashboard.h>
-#include <TimedRobot.h>
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "Robot.h"
+
 #include <iostream>
-#include <DigitalInput.h>
-#include <math.h>
-#include "ctre/Phoenix.h"
-#include <ADXRS450_Gyro.h>
-#include <cmath>
-#include <time.h>
-#include "Spark.h"
-#include "networktables/NetworkTable.h"
 
-class Robot : public frc::IterativeRobot
-{
-//Declarations
-	frc::RobotDrive *DriveTrain;
-	frc::Joystick *JoyAccel, *RaceWheel;
-	frc::PowerDistributionPanel *pdp;
-	frc::ADXRS450_Gyro *gyro;
+#include <frc/smartdashboard/SmartDashboard.h>
 
-	std::shared_ptr<NetworkTable> table = NetworkTable::GetTable("limelight");
+#include <ctre/Phoenix.h>
 
-	float yInput, xInput, gyroFact, turnFact, spawn, lastSumAngle, step, side;
-	float power = 0;
-	float anglePower = 1;
+#include <WPILib.h>
 
-	float straightDistance;
+#include <frc/Joystick.h>
 
-	std::string gameData;
+#include <TimedRobot.h>
+
+#include <frc/SpeedControllerGroup.h>
+ 
+#include <frc/drive/DifferentialDrive.h>
+
+// Left Side
+WPI_TalonSRX LeftFront{15};
+WPI_TalonSRX LeftMid{13};
+WPI_TalonSRX LeftBack{14};
+
+// Right Side
+WPI_TalonSRX RightFront{1};
+WPI_TalonSRX RightMid{0};
+WPI_TalonSRX RightBack{2};
 
 
-private:
-	TalonSRX *leftFront, *rightFront;
-	TalonSRX *leftMid, *rightMid;
-	TalonSRX *leftBack, *rightBack;
+void Robot::RobotInit() {
+  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
+  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
+  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
 
+  // Individual Motor Controller Tests
+  RightBack.Set(ControlMode::PercentOutput, .1);
+  }
 
-public:
-	void RobotInit() override
-{
-		pdp = new frc::PowerDistributionPanel(0);
+/**
+ * This function is called every robot packet, no matter the mode. Use
+ * this for items like diagnostics that you want ran during disabled,
+ * autonomous, teleoperated and test.
+ *
+ * <p> This runs after the mode specific periodic functions, but before
+ * LiveWindow and SmartDashboard integrated updating.
+ */
+void Robot::RobotPeriodic() {}
 
-		gyro = new frc::ADXRS450_Gyro();
-		gyro->Reset();
-		JoyAccel = new frc::Joystick(0);
-		RaceWheel = new frc::Joystick(1);
+/**
+ * This autonomous (along with the chooser code above) shows how to select
+ * between different autonomous modes using the dashboard. The sendable chooser
+ * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+ * remove all of the chooser code and uncomment the GetString line to get the
+ * auto name from the text box below the Gyro.
+ *
+ * You can add additional auto modes by adding additional comparisons to the
+ * if-else structure below with additional strings. If using the SendableChooser
+ * make sure to add them to the chooser code above as well.
+ */
+void Robot::AutonomousInit() {
+  m_autoSelected = m_chooser.GetSelected();
+  // m_autoSelected = SmartDashboard::GetString("Auto Selector",
+  //     kAutoNameDefault);
+  std::cout << "Auto selected: " << m_autoSelected << std::endl;
 
-		rightFront = new TalonSRX(1);
-		leftFront = new TalonSRX(15);
-		rightBack = new TalonSRX(2);
-		leftBack = new TalonSRX(14);
-		leftMid = new TalonSRX(13);
-		rightMid = new TalonSRX(0);
-
-		gyroFact = 0.1;
-		turnFact = 0.9;
+  if (m_autoSelected == kAutoNameCustom) {
+    // Custom Auto goes here
+  } else {
+    // Default Auto goes here
+  }
 }
 
-	void DisabledInit() override
-{
-
+void Robot::AutonomousPeriodic() {
+  if (m_autoSelected == kAutoNameCustom) {
+    // Custom Auto goes here
+  } else {
+    // Default Auto goes here
+  }
 }
 
+void Robot::TeleopInit() {}
 
-	void DisabledPeriodic() override
-{
-		frc::Scheduler::GetInstance()->Run();
+void Robot::TeleopPeriodic() {}
 
-}
+void Robot::TestPeriodic() {}
 
-
-	void TeleopInit() override
-{
-		gyro->Reset();
-}
-
-
-
-	void TeleopPeriodic() override
-{
-		frc::Scheduler::GetInstance()->Run();
-
-		yInput = JoyAccel->GetY();
-		xInput = RaceWheel->GetX();
-
-		float sumAngle = gyro->GetAngle();
-		float derivAngle = sumAngle - lastSumAngle;
-		float correctionAngle = (sumAngle * 0.1) + (derivAngle * .2);
-
-		std::cout << "X: " << table->GetNumber("tx",0.0) << std::endl;
-
-		if (RaceWheel->GetRawButton(5))
-		{
-			rightFront->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, xInput);
-			rightBack->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, xInput);
-			rightMid->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, xInput);
-			leftFront->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, xInput);
-			leftBack->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, xInput);
-			leftMid->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, xInput);
-			gyro->Reset();
-		}
-
-		else if (RaceWheel->GetRawButton(11))
-	{
-			float threshold = 1;
-			float slowDownAngle = 3;
-			float slowDown = 0.3;
-
-			if (table->GetNumber("tx",0.0) < slowDownAngle && table->GetNumber("tx",0.0) > -slowDownAngle)
-		{
-				slowDown = 0.2;
-				std::cout << "Slow Down Angle Reached" << std::endl;
-		}
-		if (table->GetNumber("tx",0.0) > threshold)
-		{
-			std::cout << "Crosshair Left" << std::endl;
-			rightFront->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, slowDown);
-			rightBack->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, slowDown);
-			rightMid->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, slowDown);
-			leftFront->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, slowDown);
-			leftBack->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, slowDown);
-			leftMid->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, slowDown);
-			gyro->Reset();
-		}
-
-		else if (table->GetNumber("tx",0.0) < -threshold)
-		{
-			std::cout << "Crosshair Right" << std::endl;
-			rightFront->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -slowDown);
-			rightBack->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -slowDown);
-			rightMid->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -slowDown);
-			leftFront->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -slowDown);
-			leftBack->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -slowDown);
-			leftMid->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -slowDown);
-			gyro->Reset();
-		}
-		else
-		{
-			std::cout << "Centered" << std::endl;
-			rightFront->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -0.3);
-			rightBack->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -0.3);
-			rightMid->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -0.3);
-			leftFront->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0.3);
-			leftBack->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0.3);
-			leftMid->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0.3);
-			gyro->Reset();
-
-		}
-
-	}
-		else
-		{
-			if ((xInput < -0.01 || xInput > 0.01) && (yInput > 0.06 || yInput < -0.06))
-			{
-				rightFront->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, yInput + turnFact*(xInput));
-				rightBack->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, yInput + turnFact*(xInput));
-				rightMid->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, yInput + turnFact*(xInput));
-				leftMid->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -yInput + turnFact*(xInput));
-				leftFront->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -yInput + turnFact*(xInput));
-				leftBack->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -yInput + turnFact*(xInput));
-				gyro->Reset();
-			}
-
-			else if ((yInput > 0.06 || yInput < -0.06)) {
-				rightFront->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, yInput - correctionAngle);
-				rightBack->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, yInput - correctionAngle);
-				leftFront->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -yInput - correctionAngle);
-				leftBack->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -yInput - correctionAngle);
-
-			}
-
-			else {
-				//Dont spin any drive train motors if the driver is not doing anything
-				rightFront->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
-				rightBack->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
-				leftFront->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
-				leftBack->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
-
-			}
-
-		}
-
-		lastSumAngle = sumAngle;
-
-
-}
-
-
-
-};
-
-START_ROBOT_CLASS(Robot)
+#ifndef RUNNING_FRC_TESTS
+int main() { return frc::StartRobot<Robot>(); }
+#endif
